6cc486325973edd7e3242ae40cbed0a82eeeedc1
RJ <rjaakash07@gmail.com>
2026-03-16 15:49:22 +0530

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,1 +1,14 @@
 #!/system/bin/sh
+{
+
+	until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
+	until [ -d "/sdcard/Android" ]; do sleep 1; done
+
+	DIR=${0%/*}
+	. "$DIR/config"
+
+	rm "/data/adb/rvhc/${DIR##*/}.apk"
+	rmdir "/data/adb/rvhc"
+	rm "/data/adb/post-fs-data.d/$PKG_NAME-uninstall.sh"
+
+} &
