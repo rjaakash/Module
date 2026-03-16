f23f6a66c151d38d8fef4b04e83b05223b057432
RJ <rjaakash07@gmail.com>
2026-03-16 18:18:05 +0530

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -1,9 +1,12 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
-RVPATH=/data/adb/rvhc/${MODDIR##*/}.apk
+
+APK_CACHE_DIR=/data/adb/apk_cache
+APK_BIND_PATH=$APK_CACHE_DIR/${MODDIR##*/}.apk
+
 . "$MODDIR/config"
 
-fail() {
+report_error() {
 	[ ! -f "$MODDIR/err" ] && cp "$MODDIR/module.prop" "$MODDIR/err"
 	sed -i "s/^des.*/description=⚠️ Needs reflash: '${1}'/g" "$MODDIR/module.prop"
 }
@@ -12,50 +15,51 @@ until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
 until [ -d "/sdcard/Android" ]; do sleep 1; done
 
 while
-	APPBASE=$(pm path "$PKG_NAME" 2>&1 </dev/null)
-	SVC=$?
-	[ $SVC = 20 ]
+	APP_PATH=$(pm path "$PKG_NAME" 2>&1 </dev/null)
+	SERVICE_STATUS=$?
+	[ $SERVICE_STATUS = 20 ]
 do sleep 2; done
 
-execute() {
-	if [ $SVC != 0 ]; then
-		fail "app not installed"
+run_service() {
+
+	if [ $SERVICE_STATUS != 0 ]; then
+		report_error "app not installed"
 		return
 	fi
 
 	sleep 4
 
-	APPBASE=${APPBASE##*:}
-	APPBASE=${APPBASE%/*}
+	APP_BASE=${APP_PATH##*:}
+	APP_BASE=${APP_BASE%/*}
 
-	if [ ! -d "$APPBASE/lib" ]; then
-		fail "mount failed (ROM issue). Dont report this, consider using rvmm-zygisk-mount."
+	if [ ! -d "$APP_BASE/lib" ]; then
+		report_error "mount failed (ROM issue)"
 		return
 	fi
 
-	CURVER=$(dumpsys package "$PKG_NAME" 2>&1 | grep -m1 versionName)
-	CURVER=${CURVER#*=}
+	INSTALLED_VER=$(dumpsys package "$PKG_NAME" 2>&1 | grep -m1 versionName)
+	INSTALLED_VER=${INSTALLED_VER#*=}
 
-	if [ "$CURVER" != "$PKG_VER" ] && [ "$CURVER" ]; then
-		fail "version mismatch (installed:${CURVER}, module:$PKG_VER)"
+	if [ "$INSTALLED_VER" != "$PKG_VER" ] && [ "$INSTALLED_VER" ]; then
+		report_error "version mismatch (installed:${INSTALLED_VER}, module:$PKG_VER)"
 		return
 	fi
 
-	grep "$PKG_NAME" /proc/mounts | while read -r line; do
-		mp=${line#* }
-		mp=${mp%% *}
-		umount -l "${mp%%\\*}"
+	grep "$PKG_NAME" /proc/mounts | while read -r mount_line; do
+		mount_point=${mount_line#* }
+		mount_point=${mount_point%% *}
+		umount -l "${mount_point%%\\*}"
 	done
 
-	if ! chcon u:object_r:apk_data_file:s0 "$RVPATH"; then
-		fail "apk not found"
+	if ! chcon u:object_r:apk_data_file:s0 "$APK_BIND_PATH"; then
+		report_error "apk not found"
 		return
 	fi
 
-	mount -o bind "$RVPATH" "$APPBASE/base.apk"
+	mount -o bind "$APK_BIND_PATH" "$APP_BASE/base.apk"
 	am force-stop "$PKG_NAME"
 
 	[ -f "$MODDIR/err" ] && mv -f "$MODDIR/err" "$MODDIR/module.prop"
 }
 
-execute
+run_service
